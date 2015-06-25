51e0ccea43c3fe126e407001319ca0207f666bff
Yuriy Bogdanov <chinsay@gmail.com>
2015-06-25 14:45:22 +0300

    config extend

--- src/compose/config.go
+++ src/compose/config.go
@@ -17,29 +17,29 @@ type Config struct {
 }
 
 type ConfigContainer struct {
-	Image           string
-	Extends         string
-	Net             string
-	Pid             string
-	Uts             string // TODO: find in docker remote api
-	Dns             []string
-	AddHost         []string `yaml:"add_host"`
-	Restart         string
-	Memory          string
-	MemorySwap      string `yaml:"memory_swap"`
-	CpuShares       int64  `yaml:"cpu_shares"`
-	CpusetCpus      string `yaml:"cpuset_cpus"`
-	OomKillDisable  string `yaml:"oom_kill_disable"` // TODO: pull request to go-dockerclient
-	Ulimits         []*ConfigUlimit
-	Privileged      bool
-	Cmd             []string
-	Entrypoint      []string
-	Expose          []*PortBinding
-	PublishAllPorts bool
-	Labels          map[string]string
-	VolumesFrom     []string // TODO: may be referred to another compose namespace
-	Volumes         []string
-	KillTimeout     int `yaml:"kill_timeout"`
+	Image           string            ``
+	Extends         string            ``
+	Net             string            ``
+	Pid             string            ``
+	Uts             string            `` // TODO: find in docker remote api
+	Dns             []string          ``
+	AddHost         []string          `yaml:"add_host"`
+	Restart         string            ``
+	Memory          ConfigMemory      ``
+	MemorySwap      ConfigMemorySwap  `yaml:"memory_swap"`
+	CpuShares       *int64            `yaml:"cpu_shares"`
+	CpusetCpus      string            `yaml:"cpuset_cpus"`
+	OomKillDisable  *bool             `yaml:"oom_kill_disable"` // TODO: pull request to go-dockerclient
+	Ulimits         []*ConfigUlimit   ``
+	Privileged      *bool             ``
+	Cmd             []string          ``
+	Entrypoint      []string          ``
+	Expose          []*PortBinding    ``
+	PublishAllPorts *bool             ``
+	Labels          map[string]string ``
+	VolumesFrom     []string          `` // TODO: may be referred to another compose namespace
+	Volumes         []string          ``
+	KillTimeout     *int              `yaml:"kill_timeout"`
 }
 
 type ConfigUlimit struct {
@@ -49,6 +49,10 @@ type ConfigUlimit struct {
 }
 
 type PortBinding string
+
+type ConfigMemory string
+
+type ConfigMemorySwap string
 
 func NewConfigFromApiConfig(apiConfig *docker.Config) *ConfigContainer {
 	return &ConfigContainer{}
@@ -62,7 +66,84 @@ func (config *ConfigContainer) IsEqualTo(config2 *ConfigContainer) bool {
 	return true
 }
 
-func (config *ConfigContainer) ExtendFrom(parent *ConfigContainer) {
+func (container *ConfigContainer) ExtendFrom(parent *ConfigContainer) {
+	if container.Image == "" {
+		container.Image = parent.Image
+	}
+	if container.Net == "" {
+		container.Net = parent.Net
+	}
+	if container.Pid == "" {
+		container.Pid = parent.Pid
+	}
+	if container.Uts == "" {
+		container.Uts = parent.Uts
+	}
+	if container.Dns == nil {
+		container.Dns = parent.Dns
+	}
+	if container.AddHost == nil {
+		container.AddHost = parent.AddHost
+	}
+	if container.Restart == "" {
+		container.Restart = parent.Restart
+	}
+	if container.Memory == "" {
+		container.Memory = parent.Memory
+	}
+	if container.MemorySwap == "" {
+		container.MemorySwap = parent.MemorySwap
+	}
+	if container.MemorySwap == "" {
+		container.MemorySwap = parent.MemorySwap
+	}
+	if container.CpuShares == nil {
+		container.CpuShares = parent.CpuShares
+	}
+	if container.CpusetCpus == "" {
+		container.CpusetCpus = parent.CpusetCpus
+	}
+	if container.OomKillDisable == nil {
+		container.OomKillDisable = parent.OomKillDisable
+	}
+	if container.Ulimits == nil {
+		container.Ulimits = parent.Ulimits
+	}
+	if container.Privileged == nil {
+		container.Privileged = parent.Privileged
+	}
+	if container.Cmd == nil {
+		container.Cmd = parent.Cmd
+	}
+	if container.Entrypoint == nil {
+		container.Entrypoint = parent.Entrypoint
+	}
+	if container.Expose == nil {
+		container.Expose = parent.Expose
+	}
+	if container.PublishAllPorts == nil {
+		container.PublishAllPorts = parent.PublishAllPorts
+	}
+	// Extend labels
+	newLabels := make(map[string]string)
+	for k, v := range parent.Labels {
+		newLabels[k] = v
+	}
+	for k, v := range container.Labels {
+		newLabels[k] = v
+	}
+	container.Labels = newLabels
+
+	if container.VolumesFrom == nil {
+		container.VolumesFrom = parent.VolumesFrom
+	}
+	if container.Volumes == nil {
+		container.Volumes = parent.Volumes
+	}
+	if container.KillTimeout == nil {
+		container.KillTimeout = parent.KillTimeout
+	}
+
 	return
 }
 
@@ -88,7 +169,23 @@ func ReadConfig(reader io.Reader) (*Config, error) {
 		return nil, fmt.Errorf("Failed to parse YAML config, error: %s", err)
 	}
 
-	// TODO: make extends
+	// Process extending containers configuration
+	for name, container := range config.Containers {
+		if container.Extends != "" {
+			if container.Extends == name {
+				return nil, fmt.Errorf("Container %s: cannot extend from itself", name)
+			}
+			if _, ok := config.Containers[container.Extends]; !ok {
+				return nil, fmt.Errorf("Container %s: cannot find container %s to extend from", name, container.Extends)
+			}
+			// TODO: build dependency graph by extends hierarchy to allow multiple inheritance
+			if config.Containers[container.Extends].Extends != "" {
+				return nil, fmt.Errorf("Container %s: cannot extend from %s: multiple inheritance is not allowed yet",
+					name, container.Extends)
+			}
+			container.ExtendFrom(config.Containers[container.Extends])
+		}
+	}
 
 	return config, nil
 }
--- src/compose/config_test.go
+++ src/compose/config_test.go
@@ -1,7 +1,6 @@
 package compose
 
 import (
-	// "fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -27,9 +26,29 @@ func TestConfigExtend(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// fmt.Printf("config: %q\n", config)
-
 	// TODO: more config assertions
 	assert.Equal(t, "patterns", config.Namespace)
 	assert.Equal(t, "dockerhub.grammarly.io/patterns:{{patterns_version}}", config.Containers["main2"].Image)
+
+	// should be inherited
+	assert.Equal(t, []string{"8.8.8.8"}, config.Containers["main2"].Dns)
+	// should be overriden
+	assert.Equal(t, []string{"capi.grammarly.com:127.0.0.2"}, config.Containers["main2"].AddHost)
+
+	// should be inherited
+	assert.EqualValues(t, 512, *config.Containers["main2"].CpuShares)
+
+	// should inherit and merge labels
+	assert.Equal(t, 3, len(config.Containers["main2"].Labels))
+	assert.Equal(t, "pattern", config.Containers["main2"].Labels["service"])
+	assert.Equal(t, "2", config.Containers["main2"].Labels["num"])
+	assert.Equal(t, "replica", config.Containers["main2"].Labels["type"])
+
+	// should not affect parent labels
+	assert.Equal(t, 2, len(config.Containers["main"].Labels))
+	assert.Equal(t, "pattern", config.Containers["main"].Labels["service"])
+	assert.Equal(t, "1", config.Containers["main"].Labels["num"])
+
+	// should be overriden
+	assert.EqualValues(t, 200, *config.Containers["main2"].KillTimeout)
 }
