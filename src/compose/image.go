a23c11cb056ec98fcb940af3e2cfffef2ee5075c
Yuriy Bogdanov <chinsay@gmail.com>
2015-06-26 09:55:01 +0300

    todo

--- src/compose/image.go
+++ src/compose/image.go
@@ -27,6 +27,7 @@ func NewImageNameFromString(image string) *ImageName {
 	split := strings.SplitN(image, ":", 2)
 	// TODO: do we allow dots "." in image name?
 	// if yes, find another way to distinguish registry from account name part
+	// Maybe, borrow this: https://github.com/jwilder/docker-gen/blob/82da74aaa33dbc9bf96ebf53a11ce289e672dbf2/docker_client.go#L77
 	if strings.Contains(split[0], ".") {
 		registryAndName := strings.SplitN(split[0], "/", 2)
 		dockerImage.Registry = registryAndName[0]
