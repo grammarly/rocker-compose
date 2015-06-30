a299919954c5648e5cbde4041791930bde3af3fe
Yuriy Bogdanov <chinsay@gmail.com>
2015-06-30 12:20:58 +0300

    main.go future stuff

--- src/cmd/rocker-compose/main.go
+++ src/cmd/rocker-compose/main.go
@@ -27,7 +27,24 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// if c.GlobalIsSet("tlsverify") {
+	//   config.tlsverify = c.GlobalBool("tlsverify")
+	//   config.tlscacert = globalString(c, "tlscacert")
+	//   config.tlscert = globalString(c, "tlscert")
+	//   config.tlskey = globalString(c, "tlskey")
+	// }
+
 	fmt.Printf("Config path: %s\n", configFilename)
 
 	fmt.Printf("Config: %+q\n", config)
 }
+
+// Fix string arguments enclosed with boudle quotes
+// 'docker-machine config' gives such arguments
+// func globalString(c *cli.Context, name string) string {
+// 	str := c.GlobalString(name)
+// 	if len(str) >= 2 && str[0] == '\u0022' && str[len(str)-1] == '\u0022' {
+// 		str = str[1 : len(str)-1]
+// 	}
+// 	return str
+// }
